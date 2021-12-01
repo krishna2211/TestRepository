fb5cb7660bc9ee5ea2598c5aad03f122d8b6062c
Robin <krishnadeshpande141@gmail.com>
2021-12-01 14:47:00 +0530

    Hello My Git.

---
+++ MyConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am pushing application to git");
+        }
+
+    }
+}
